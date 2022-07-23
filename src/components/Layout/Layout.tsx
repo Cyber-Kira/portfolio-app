a91482129b60fb0ad32c67324cd768aed7da334a
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-07-23 13:21:43 +0300

    feat: add about component to main"

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { About } from '../About'
 
 export const Layout = () => {
-	return <div>Layout</div>
+	return (
+		<main className='max-w-5xl mx-auto flex px-3 md:px-0 mt-[4.375rem]'>
+			<About />
+		</main>
+	)
 }
