df0d660c865fa5d70b97668433a7a3ad69f3e94a
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-07-27 12:02:40 +0300

    refactoring: change order of projects

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -23,16 +23,6 @@ const latestProject = {
 const projects = [
 	{
 		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
-		title: '#Todo',
-		description:
-			'A Todo application. Did a lot of work with CSS animations. Best works on mobile',
-		imageLink:
-			'https://i.ibb.co/fQJ62q9/screencapture-todo-3cbda-web-app-2022-07-26-22-39-33.png',
-		demoLink: 'https://todo-3cbda.web.app',
-		sourceLink: 'https://github.com/Cyber-Kira/todo',
-	},
-	{
-		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
 		title: 'Weather App',
 		description:
 			'An application to display weather for current location or using search. Using openweather API.',
@@ -43,58 +33,23 @@ const projects = [
 	},
 	{
 		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
+		title: '#Todo',
+		description:
+			'A Todo application. Did a lot of work with CSS animations. Best works on mobile',
+		imageLink:
+			'https://i.ibb.co/fQJ62q9/screencapture-todo-3cbda-web-app-2022-07-26-22-39-33.png',
+		demoLink: 'https://todo-3cbda.web.app',
+		sourceLink: 'https://github.com/Cyber-Kira/todo',
+	},
+
+	{
+		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
 		title: 'Windbnb',
 		description: 'An application to render stays with sorting.',
 		imageLink:
 			'https://i.ibb.co/2qgBsbL/Screenshot-from-2022-07-26-22-45-11.png',
 		demoLink: 'https://windbnb-ae8e5.web.app',
 		sourceLink: 'https://github.com/Cyber-Kira/windbnb',
-	},
-	{
-		tags: ['HTML', 'CSS'],
-		title: '404 Not Found',
-		description: 'A 404 not found page with responsive design.',
-		imageLink:
-			'https://i.ibb.co/ydrJ7H3/screencapture-cyber-kira-github-io-404-not-found-2022-01-12-10-39-19.png',
-		demoLink: 'https://cyber-kira.github.io/404-not-found',
-		sourceLink: 'https://github.com/Cyber-Kira/404-not-found',
-	},
-	{
-		tags: ['JavaScript'],
-		title: 'Brackets',
-		description:
-			'Implement function `check(str, bracketsConfig)`, that for given brackets sequence will return `true` if it is correct and `false` otherwise.',
-		imageLink:
-			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
-		demoLink: '',
-		sourceLink: 'https://github.com/Cyber-Kira/brackets',
-	},
-	{
-		tags: ['React', 'Typescript', 'SASS'],
-		title: 'Button Component',
-		description:
-			'A reusable button with all the states in the design and a page displaying all the states.',
-		imageLink: 'https://i.ibb.co/B3DrHBj/1.png',
-		demoLink: 'https://devchallenges-63e63.web.app/',
-		sourceLink: 'https://github.com/Cyber-Kira/button-component',
-	},
-	{
-		tags: ['Javascript', 'Course'],
-		title: 'Arkanoid',
-		description: 'Arkanoid game written on Javascript.',
-		imageLink:
-			'https://i.ibb.co/yFHs9Sm/screencapture-cyber-kira-github-io-canvas-arkanoid-2022-07-26-20-00-30.png',
-		demoLink: 'https://cyber-kira.github.io/canvas-arkanoid/',
-		sourceLink: 'https://github.com/Cyber-Kira/canvas-arkanoid',
-	},
-	{
-		tags: ['HTML', 'SASS', 'Javascript'],
-		title: 'Checkout Page',
-		description: 'A checkout page with responsive design.',
-		imageLink:
-			'https://i.ibb.co/4fg2Ndx/screencapture-cyber-kira-github-io-checkout-page-2022-02-19-17-51-07.png',
-		demoLink: 'https://cyber-kira.github.io/checkout-page',
-		sourceLink: 'https://cyber-kira.github.io/checkout-page',
 	},
 	{
 		tags: ['React', 'Typescript', 'Tailwind'],
@@ -106,6 +61,34 @@ const projects = [
 		sourceLink: 'https://github.com/Cyber-Kira/country-quiz',
 	},
 	{
+		tags: ['React', 'Typescript', 'SASS'],
+		title: 'Button Component',
+		description:
+			'A reusable button with all the states in the design and a page displaying all the states.',
+		imageLink: 'https://i.ibb.co/B3DrHBj/1.png',
+		demoLink: 'https://devchallenges-63e63.web.app/',
+		sourceLink: 'https://github.com/Cyber-Kira/button-component',
+	},
+	{
+		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
+		title: 'Quote Generator',
+		description:
+			'A quote generator application with responsive design. Using Quote Garden API.',
+		imageLink:
+			'https://i.ibb.co/2kD2zZm/screencapture-quote-generator-69e0d-web-app-2022-07-26-21-06-31.png',
+		demoLink: 'https://quote-generator-69e0d.web.app/',
+		sourceLink: 'https://github.com/Cyber-Kira/quote-generator',
+	},
+	{
+		tags: ['HTML', 'SASS', 'Javascript'],
+		title: 'Checkout Page',
+		description: 'A checkout page with responsive design.',
+		imageLink:
+			'https://i.ibb.co/4fg2Ndx/screencapture-cyber-kira-github-io-checkout-page-2022-02-19-17-51-07.png',
+		demoLink: 'https://cyber-kira.github.io/checkout-page',
+		sourceLink: 'https://cyber-kira.github.io/checkout-page',
+	},
+	{
 		tags: ['HTML', 'SASS'],
 		title: 'Edie Homepage',
 		description: "Single page Edie's startup page with responsive design.",
@@ -115,15 +98,6 @@ const projects = [
 		sourceLink: 'https://github.com/Cyber-Kira/edie-homepage',
 	},
 	{
-		tags: ['Javascript'],
-		title: 'Guessing Game',
-		description: 'A guessing game class which using binary search algorithm.',
-		imageLink:
-			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
-		demoLink: '',
-		sourceLink: 'https://github.com/Cyber-Kira/guessing-game',
-	},
-	{
 		tags: ['HTML', 'SASS', 'Javascript'],
 		title: 'Interior Consultant',
 		description: 'An Interior Consultant homepage with responsive design.',
@@ -131,24 +105,6 @@ const projects = [
 			'https://i.ibb.co/4Zj5cCr/screencapture-127-0-0-1-5500-2022-02-09-15-10-24.png',
 		demoLink: 'https://cyber-kira.github.io/interior-consultant',
 		sourceLink: 'https://github.com/Cyber-Kira/interior-consultant',
-	},
-	{
-		tags: ['Javascript'],
-		title: 'Mind Games',
-		description: 'Set of games that runs in the console.',
-		imageLink:
-			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
-		demoLink: '',
-		sourceLink: 'https://github.com/Cyber-Kira/Mind-games',
-	},
-	{
-		tags: ['Javascript'],
-		title: 'Morse Decoder',
-		description: 'A function, that decodes Morse code and returns a string.',
-		imageLink:
-			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
-		demoLink: '',
-		sourceLink: 'https://github.com/Cyber-Kira/morse-decoder',
 	},
 	{
 		tags: ['HTML', 'SASS'],
@@ -169,6 +125,24 @@ const projects = [
 		sourceLink: 'https://github.com/Cyber-Kira/my-team-page',
 	},
 	{
+		tags: ['HTML', 'SASS'],
+		title: 'Recipe Page.',
+		description: 'Single Page recipe blog with responsive design.',
+		imageLink:
+			'https://i.ibb.co/NSb0Dtq/Screenshot-from-2022-07-26-22-31-03.png',
+		demoLink: 'https://cyber-kira.github.io/recipe-page/',
+		sourceLink: 'https://github.com/Cyber-Kira/recipe-page',
+	},
+	{
+		tags: ['HTML', 'CSS'],
+		title: '404 Not Found',
+		description: 'A 404 not found page with responsive design.',
+		imageLink:
+			'https://i.ibb.co/ydrJ7H3/screencapture-cyber-kira-github-io-404-not-found-2022-01-12-10-39-19.png',
+		demoLink: 'https://cyber-kira.github.io/404-not-found',
+		sourceLink: 'https://github.com/Cyber-Kira/404-not-found',
+	},
+	{
 		tags: ['HTML', 'SASS', 'Javascript'],
 		title: 'Productly',
 		description: 'A single page design.',
@@ -176,16 +150,6 @@ const projects = [
 			'https://i.ibb.co/1dSC3HY/Screenshot-from-2022-07-26-21-03-44.png',
 		demoLink: 'https://cyber-kira.github.io/productly-landing-page/',
 		sourceLink: 'https://github.com/Cyber-Kira/productly-landing-page',
-	},
-	{
-		tags: ['React', 'Typescript', 'Redux', 'Tailwind'],
-		title: 'Quote Generator',
-		description:
-			'A quote generator application with responsive design. Using Quote Garden API.',
-		imageLink:
-			'https://i.ibb.co/2kD2zZm/screencapture-quote-generator-69e0d-web-app-2022-07-26-21-06-31.png',
-		demoLink: 'https://quote-generator-69e0d.web.app/',
-		sourceLink: 'https://github.com/Cyber-Kira/quote-generator',
 	},
 	{
 		tags: ['React', 'Javascript', 'Course'],
@@ -208,15 +172,6 @@ const projects = [
 		sourceLink: 'https://github.com/Cyber-Kira/react-star-wars-app',
 	},
 	{
-		tags: ['HTML', 'SASS'],
-		title: 'Recipe Page.',
-		description: 'Single Page recipe blog with responsive design.',
-		imageLink:
-			'https://i.ibb.co/NSb0Dtq/Screenshot-from-2022-07-26-22-31-03.png',
-		demoLink: 'https://cyber-kira.github.io/recipe-page/',
-		sourceLink: 'https://github.com/Cyber-Kira/recipe-page',
-	},
-	{
 		tags: ['React', 'Redux', 'Javascript', 'Course'],
 		title: 'ReStore.',
 		description: 'Simple store. First time using Redux.',
@@ -224,6 +179,52 @@ const projects = [
 			'https://i.ibb.co/0XL1S4G/screencapture-redux-store-3512-web-app-2022-07-26-22-34-20.png',
 		demoLink: 'https://redux-store-3512.web.app/',
 		sourceLink: 'https://github.com/Cyber-Kira/redux-store',
+	},
+	{
+		tags: ['Javascript', 'Course'],
+		title: 'Arkanoid',
+		description: 'Arkanoid game written on Javascript.',
+		imageLink:
+			'https://i.ibb.co/yFHs9Sm/screencapture-cyber-kira-github-io-canvas-arkanoid-2022-07-26-20-00-30.png',
+		demoLink: 'https://cyber-kira.github.io/canvas-arkanoid/',
+		sourceLink: 'https://github.com/Cyber-Kira/canvas-arkanoid',
+	},
+	{
+		tags: ['Javascript'],
+		title: 'Guessing Game',
+		description: 'A guessing game class which using binary search algorithm.',
+		imageLink:
+			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
+		demoLink: '',
+		sourceLink: 'https://github.com/Cyber-Kira/guessing-game',
+	},
+	{
+		tags: ['Javascript'],
+		title: 'Mind Games',
+		description: 'Set of games that runs in the console.',
+		imageLink:
+			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
+		demoLink: '',
+		sourceLink: 'https://github.com/Cyber-Kira/Mind-games',
+	},
+	{
+		tags: ['Javascript'],
+		title: 'Morse Decoder',
+		description: 'A function, that decodes Morse code and returns a string.',
+		imageLink:
+			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
+		demoLink: '',
+		sourceLink: 'https://github.com/Cyber-Kira/morse-decoder',
+	},
+	{
+		tags: ['JavaScript'],
+		title: 'Brackets',
+		description:
+			'Implement function `check(str, bracketsConfig)`, that for given brackets sequence will return `true` if it is correct and `false` otherwise.',
+		imageLink:
+			'https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69',
+		demoLink: '',
+		sourceLink: 'https://github.com/Cyber-Kira/brackets',
 	},
 ]
 
